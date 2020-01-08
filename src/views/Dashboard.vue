36acff3f6e16f103f6c5ef32f4da426de2e540cb
Kenan Erarslan <kenan@enginaar.com>
2020-01-08 22:47:00 +0300

    v-layout is deprecated, v-row used sorting added

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,28 +2,48 @@
   <div class="home">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-       <v-card flat v-for="project in projects" :key="project.title">
-         <v-layout wrap :class="`pa-3 project ${project.status}`">
-           <v-flex xs12 md6>
-             <div class="caption grey--text">Project title</div>
-             <div>{{project.title}}</div>
-           </v-flex>
-           <v-flex xs6 sm4 md2>
-              <div class="caption grey--text">Person</div>
-              <div>{{project.person}}</div>
-           </v-flex>
-            <v-flex xs2 sm4 md2>
-              <div class="caption grey--text">Due Date</div>
-              <div>{{project.due}}</div>
-           </v-flex>
-            <v-flex xs2 sm4 md2>
-              <div>
-                <v-chip small :class="`${project.status} white--text caption`">{{project.status}}</v-chip>
-              </div>
-           </v-flex>
-         </v-layout>
-         <v-divider></v-divider>
-       </v-card>
+      <v-row class="mb-3">
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small text color="grey" @click="sortBy('title')" v-on="on">
+              <v-icon left small>mdi-folder</v-icon>
+              <span class="caption text-lowercase">By Project Name</span>
+            </v-btn>
+          </template>
+          <span>Sort projects by project names</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small text color="grey" @click="sortBy('person')" v-on="on">
+              <v-icon left small>mdi-account</v-icon>
+              <span class="caption text-lowercase">By Person</span>
+            </v-btn>
+          </template>
+          <span>Sort projects by person</span>
+        </v-tooltip>
+      </v-row>
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-row wrap :class="`pa-3 project ${project.status}`">
+          <v-col xs12 md6>
+            <div class="caption grey--text">Project title</div>
+            <div>{{project.title}}</div>
+          </v-col>
+          <v-col xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{project.person}}</div>
+          </v-col>
+          <v-col xs2 sm4 md2>
+            <div class="caption grey--text">Due Date</div>
+            <div>{{project.due}}</div>
+          </v-col>
+          <v-col xs2 sm4 md2>
+            <div>
+              <v-chip small :class="`${project.status} white--text caption`">{{project.status}}</v-chip>
+            </div>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -33,41 +53,74 @@ export default {
   data() {
     return {
       projects: [
-        {title: "Title 1", person: "Person 1", due: "1st Jan 2020", status: "ongoing", content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, perferendis quisquam! Nemo corporis, velit, placeat perspiciatis sapiente temporibus maxime consequuntur optio tempore eos iusto, repudiandae nihil! Sint nesciunt molestiae suscipit."},
-        {title: "Title 2", person: "Person 2", due: "2st Jan 2020", status: "complete", content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, perferendis quisquam! Nemo corporis, velit, placeat perspiciatis sapiente temporibus maxime consequuntur optio tempore eos iusto, repudiandae nihil! Sint nesciunt molestiae suscipit."},
-        {title: "Title 3", person: "Person 3", due: "3st Jan 2020", status: "complete", content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, perferendis quisquam! Nemo corporis, velit, placeat perspiciatis sapiente temporibus maxime consequuntur optio tempore eos iusto, repudiandae nihil! Sint nesciunt molestiae suscipit."},
-        {title: "Title 4", person: "Person 4", due: "4st Jan 2020", status: "overdue", content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, perferendis quisquam! Nemo corporis, velit, placeat perspiciatis sapiente temporibus maxime consequuntur optio tempore eos iusto, repudiandae nihil! Sint nesciunt molestiae suscipit."}
+        {
+          title: "Enginaar todo 1",
+          person: "kenan",
+          due: "1st Jan 2020",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, perferendis quisquam! Nemo corporis, velit, placeat perspiciatis sapiente temporibus maxime consequuntur optio tempore eos iusto, repudiandae nihil! Sint nesciunt molestiae suscipit."
+        },
+        {
+          title: "Seçiniz UI",
+          person: "Person 2",
+          due: "2st Jan 2020",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, perferendis quisquam! Nemo corporis, velit, placeat perspiciatis sapiente temporibus maxime consequuntur optio tempore eos iusto, repudiandae nihil! Sint nesciunt molestiae suscipit."
+        },
+        {
+          title: "Hasscript sorular",
+          person: "Herkes",
+          due: "3st Jan 2020",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, perferendis quisquam! Nemo corporis, velit, placeat perspiciatis sapiente temporibus maxime consequuntur optio tempore eos iusto, repudiandae nihil! Sint nesciunt molestiae suscipit."
+        },
+        {
+          title: "Enginaar.com blog",
+          person: "admin",
+          due: "4st Jan 2020",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, perferendis quisquam! Nemo corporis, velit, placeat perspiciatis sapiente temporibus maxime consequuntur optio tempore eos iusto, repudiandae nihil! Sint nesciunt molestiae suscipit."
+        }
       ]
+    };
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
     }
   }
-}
+};
 </script>
 
 <style>
-.project.complete{
-  border-left:4px solid #3cd2c2;
+.project {
+  margin-left: 0;
+}
+.project.complete {
+  border-left: 4px solid #3cd2c2;
 }
 
-.project.ongoing{
-  border-left:4px solid orange;
+.project.ongoing {
+  border-left: 4px solid orange;
 }
 
-.project.overdue{
-  border-left:4px solid tomato;
+.project.overdue {
+  border-left: 4px solid tomato;
 }
 
-.v-chip.complete{
-  background:#3cd1c2!important;
+.v-chip.complete {
+  background: #3cd1c2 !important;
 }
 
-.v-chip.ongoing{
-   background: orange!important;
+.v-chip.ongoing {
+  background: orange !important;
 }
 
-.v-chip.overdue{
-   background:tomato!important;
+.v-chip.overdue {
+  background: tomato !important;
 }
-
-
-
 </style>
